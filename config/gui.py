849af9bc6b4c8562d82bf67083a616c5b57c1491
slongwell <longwell@stanford.edu>
2017-05-24 10:27:09 -0700

    gui video, manifold

--- config/gui.py
+++ config/gui.py
@@ -1,26 +1,226 @@
 from IPython import display
 from ipywidgets import widgets
+import matplotlib.pyplot as plt
 import cv2
+import numpy as np
+import time
 
-def video(core):
-    import cv2
-    cv2.namedWindow('Video')
-    core.startContinuousSequenceAcquisition(1)
-    while True:
-        img = core.getLastImage()
-        if core.getRemainingImageCount() > 0:
-    #         img = core.popNextImage()
-            img = core.getLastImage()
-            cv2.imshow('Video', img)
-            cv2.waitkey(0)
-        else:
-            print('No frame')
-        if cv2.waitKey(20) >= 0:
-            break
-    core.stopSequenceAcquisition()
-    cv2.destroyAllWindows()
+# --------------------------------------------------------------------  
+# SNAP ---------------------------------------------------------------
+def snap(core, mode='mpl'):    
+    def on_snap_mpl(b):
+        core.snapImage()
+        img = core.getImage()
+        plt.imshow(img,cmap='gray')
+
+    def on_snap_cv2(b):
+        cv2.destroyWindow('Snap')
+        cv2.namedWindow('Snap',cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Snap', 500,500)
+        cv2.setWindowProperty('Snap', cv2.WND_PROP_ASPECT_RATIO, cv2.WINDOW_KEEPRATIO)
+        core.snapImage()
+        time.sleep(.1)
+        img = core.getImage()
+        cv2.imshow('Snap',img)
+        k = cv2.waitKey(30)
+
+    def on_close_cv2(b):
+        cv2.destroyWindow('Snap')
+
+        
+    snap = widgets.Button(description='Snap')
+    
+    if mode=='cv2':
+        snap.on_click(on_snap_cv2)
+        close = widgets.Button(description='Close')
+        close.on_click(on_close_cv2)
+        display.display(widgets.HBox([snap, close]))
+    else: # mode=='mpl'
+        snap.on_click(on_snap_mpl)
+        display.display(snap)
+
+
+
+# --------------------------------------------------------------------  
+# VIDEO ------------------------------------------------------
+def video(core, loop_pause=0.15):
+    # video with button (CV2)
+    live = widgets.Button(description='Live')
+    close = widgets.Button(description='Close')
+    display.display(widgets.HBox([live, close]))
+ 
+    def on_live(b):
+        display.clear_output(wait=True)
+        print 'LIVE'
+        core.startContinuousSequenceAcquisition(1000) # time overridden by exposure
+        time.sleep(.2)
+        cv2.namedWindow('Video', cv2.WINDOW_NORMAL)
+        cv2.setWindowProperty('Video', cv2.WND_PROP_ASPECT_RATIO, cv2.WINDOW_KEEPRATIO)
+        cv2.resizeWindow('Video', 500,500)
+        img = np.zeros((500,500))
+        print 'To stop, click window + press ESC'
+        while(1):
+            time.sleep(loop_pause)
+            if core.getRemainingImageCount() > 0:
+                img = core.getLastImage()
+            cv2.imshow('Video',img)
+            k = cv2.waitKey(30)
+            if k==27: # ESC key; may need 255 mask?
+                break
+        print 'STOPPED'
+        core.stopSequenceAcquisition()
+
+    def on_close(b):
+        if core.isSequenceRunning():
+            core.stopSequenceAcquisition()
+        cv2.destroyWindow('Video')
+
+        
+      
+    live.on_click(on_live)
+    close.on_click(on_close)
+
+
+    # video with button (CV2)
+    # serial snap image
+    
+    # live = widgets.Button(description='Live')
+    # close = widgets.Button(description='Close')
+    # display.display(widgets.HBox([live, close]))
+
+    # def on_live_clicked(b):
+    #     display.clear_output(wait=True)
+    #     print 'LIVE'
+    #     cv2.namedWindow('Video', cv2.WINDOW_NORMAL)
+    #     cv2.setWindowProperty('Video', cv2.WND_PROP_ASPECT_RATIO, cv2.WINDOW_KEEPRATIO)
+    #     cv2.resizeWindow('Video', 500,500)
+    #     img = np.zeros((500,500))
+    #     print 'To stop, click window + press ESC'
+    #     while(1):
+    #         core.snapImage()
+    #         time.sleep(.05)
+    #         img = core.getImage()
+    #         cv2.imshow('Video',img)
+    #         k = cv2.waitKey(30)
+    #         if k==27: # ESC key; may need 255 mask?
+    #             break
+    #     print 'STOPPED'
+
+    # def on_close_clicked(b):
+    #     if core.isSequenceRunning():
+    #         core.stopSequenceAcquisition()
+    #     cv2.destroyWindow('Video')
+
+    # live.on_click(on_live_clicked)
+    # close.on_click(on_close_clicked)
     
 
+# --------------------------------------------------------------------  
+# GRID ------------------------------------------------------
+def grid(core, loop_pause=0.15):
+    # video with button (CV2)
+
+ 
+    def on_live(b):
+        display.clear_output(wait=True)
+        print 'LIVE'
+        core.startContinuousSequenceAcquisition(1000) # time overridden by exposure
+        time.sleep(.2)
+        cv2.namedWindow('Video', cv2.WINDOW_NORMAL)
+        cv2.setWindowProperty('Video', cv2.WND_PROP_ASPECT_RATIO, cv2.WINDOW_KEEPRATIO)
+        cv2.resizeWindow('Video', 500,500)
+        img = np.zeros((500,500))
+        print 'To stop, click window + press ESC'
+        while(1):
+            time.sleep(loop_pause)
+            if core.getRemainingImageCount() > 0:
+                img = core.getLastImage()
+            cv2.imshow('Video',img)
+            k = cv2.waitKey(30)
+            if k==27: # ESC key; may need 255 mask?
+                break
+        print 'STOPPED'
+        core.stopSequenceAcquisition()
+
+    def on_close(b):
+        if core.isSequenceRunning():
+            core.stopSequenceAcquisition()
+        cv2.destroyWindow('Video')
+    
+    
+    def on_corner(b):
+        pos = core.get_xy()
+        b.owner.value = pos
+        print pos
+    
+    
+    live = widgets.Button(description='Live')
+    close = widgets.Button(description='Close')
+    
+    c1 = widgets.Button(description='Corner 1') 
+    c2 = widgets.Button(description='Corner 2')
+    = widgets.Text()
+    save = widgets.Text()
+    
+    live.on_click(on_live)
+    close.on_click(on_close)
+    
+    c1.on_click(on_corner)
+    c2.on_click(on_corner)
+    
+    # calculate: c1.value - c2.value
+    save.on_click(icon='floppy-o')
+    
+    display.display(widgets.HBox([live, close]))
+
+
+        
+# --------------------------------------------------------------------  
+# MANIFOLD -----------------------------------------------------------
+def manifold_control(manifold):
+    def on_clicked(b):
+        if b.new == True:
+            manifold.pressurize(b.owner.valve)
+        elif b.new == False:
+            manifold.depressurize(b.owner.valve)
+        else:
+            pass
+        
+    def sync(b):
+        for button in button_list:
+            button.value = manifold.read_valve(button.valve)
+    
+    button_list = []
+    for i in range(48):
+        style='info'
+        desc = '{} {}'.format(i, manifold.valvemap['name'][i])
+        
+        button_list.append(
+            widgets.ToggleButton(
+                valve = i,
+                description = desc,
+                value = manifold.read_valve(i),
+                button_style = style # 'success', 'info', 'warning', 'danger' or ''
+        ))
+        
+        
+    for button in button_list:
+        button.observe(on_clicked)
+    
+    bank_list = []
+    for i in range(0,48,8):
+        bank_list.append(
+            widgets.VBox(button_list[i:i+8][::-1])
+        )
+
+    sync_button = widgets.Button(icon='retweet', button_style='success', layout=widgets.Layout(width='40px'))
+    sync_button.on_click(sync)
+    display.display(widgets.HBox(bank_list + [sync_button] ))
+    display.display(widgets.Label('Dark = Pressurized', layout=widgets.Layout(width='300px')))
+        
+
+# --------------------------------------------------------------------  
+# STAGE CONTROL ------------------------------------------------------
 def stage_control(XY, Z):
     # icons are from "font-awesome"
     x_minus = widgets.Button(
